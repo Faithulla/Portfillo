1482e8c6123fe775043fead4bbce2992ec439cd6
Faithulla <fayzullashakirov01@gmail.com>
2021-08-18 14:44:12 +0500

    portfilio page is in progeress

--- src/Page/Portfilio/Portfilio.jsx
+++ src/Page/Portfilio/Portfilio.jsx
@@ -2,7 +2,40 @@ import './Porfilio.scss'
 export default function Portfilio() {
     return (
         <div className='Portfilio' id='portfilio'>
-            
+            <h1>Portfilio</h1>
+            <ul>
+                <li className='active'>Featured</li>
+                <li>Web App</li>
+                <li>Mobile App</li>
+                <li>Design</li>
+                <li>Branding</li>
+            </ul>
+            <div className="container">
+                <div className="item" >
+                    <img src="https://cdn.dribbble.com/users/2768987/screenshots/14668214/media/5c145c66d071199cb0a745c7983519d3.png?compress=1&resize=1600x1200" alt="" />
+                    <h3>Ranking App</h3>
+                </div>
+                <div className="item">
+                    <img src="https://cdn.dribbble.com/users/2768987/screenshots/14668214/media/5c145c66d071199cb0a745c7983519d3.png?compress=1&resize=1600x1200" alt="" />
+                    <h3>Ranking App</h3>
+                </div>
+                <div className="item">
+                    <img src="https://cdn.dribbble.com/users/2768987/screenshots/14668214/media/5c145c66d071199cb0a745c7983519d3.png?compress=1&resize=1600x1200" alt="" />
+                    <h3>Ranking App</h3>
+                </div>
+                <div className="item">
+                    <img src="https://cdn.dribbble.com/users/2768987/screenshots/14668214/media/5c145c66d071199cb0a745c7983519d3.png?compress=1&resize=1600x1200" alt="" />
+                    <h3>Ranking App</h3>
+                </div>
+                <div className="item">
+                    <img src="https://cdn.dribbble.com/users/2768987/screenshots/14668214/media/5c145c66d071199cb0a745c7983519d3.png?compress=1&resize=1600x1200" alt="" />
+                    <h3>Ranking App</h3>
+                </div>
+                <div className="item">
+                    <img src="https://cdn.dribbble.com/users/2768987/screenshots/14668214/media/5c145c66d071199cb0a745c7983519d3.png?compress=1&resize=1600x1200" alt="" />
+                    <h3>Ranking App</h3>
+                </div>
+            </div>
         </div>
     )
 }
