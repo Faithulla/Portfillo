3c17ab9822cc334b5173949aed2868c92ae1cb15
Faithulla <fayzullashakirov01@gmail.com>
2021-08-19 14:26:55 +0500

    Slider is finished

--- src/Page/Works/Works.jsx
+++ src/Page/Works/Works.jsx
@@ -1,9 +1,80 @@
-import  './Works.scss'
+import "./Works.scss";
+import ArrowRight from "../../Assets/Img/Arrow-right.png";
+import ArrowLeft from "../../Assets/Img/Arrow-left.png";
+import Project from "../../Assets/Img/workflow.png";
+import { useState } from "react";
+// import Mobile from '../../Assets/Img/mobile.png'
+// import Globe from '../../Assets/Img/globe.png'
+// import Writing from '../../Assets/Img/writing.png'
 
 export default function Works() {
-    return (
-        <div className='Works' id='works'>
-            
-        </div>
-    )
+  const handleClick = (way) => {
+    way === "left"
+      ? setcurrentSlider(currentSlider > 0 ? currentSlider - 1 : 2)
+      : setcurrentSlider(currentSlider < data.length -1 ? currentSlider + 1 : 0);
+  };
+  const [currentSlider, setcurrentSlider] = useState(0);
+  const data = [
+    {
+      id: "1",
+      icon: "../../Assets/Img/mobile.png",
+      title: "Web Design",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+      img: "https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930",
+    },
+    {
+      id: "2",
+      icon: "Assets/Img/globe.png",
+      title: "Mobile Application",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      img: "https://i.pinimg.com/originals/e9/c9/2f/e9c92f7869d682a6fa5a97fb8a298f30.jpg",
+    },
+    {
+      id: "3",
+      icon: "Assets/Img/writing.png",
+      title: "Branding",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      img: "https://i.pinimg.com/originals/a9/f6/94/a9f69465d972a004ad581f245d6ad581.jpg",
+    },
+  ];
+  return (
+    <div className="Works" id="works">
+      <div
+        className="slider"
+        style={{ transform: `translateX(-${currentSlider * 100}vw)` }}
+      >
+        {data.map((d) => (
+          <div className="container">
+            <div className="item">
+              <div className="left">
+                <div className="leftContainer">
+                  <div className="imgContainer">
+                    <img src={Project} alt="" />
+                  </div>
+                  <h2>{d.title}</h2>
+                  <p>{d.desc}</p>
+                  <span>Projects</span>
+                </div>
+              </div>
+              <div className="right">
+                <img src={d.img} alt="" />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <img
+        src={ArrowLeft}
+        alt=""
+        className="arrow left"
+        onClick={() => handleClick("left")}
+      />
+      <img
+        src={ArrowRight}
+        alt=""
+        className="arrow right"
+        onClick={() => handleClick()}
+      />
+    </div>
+  );
 }
